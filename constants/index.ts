111e778cadf2780fe122034bfac2a6ba88c4218d
windragon0807 <tmdfyd1995@gmail.com>
2025-08-27 23:30:59 +0900

    fix: 버전 업데이트

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 const HomeURL = "https://open-run.vercel.app/";
-const DevURL = "https://944d58aec2a2.ngrok.app/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
+const DevURL = "https://0410a1a29957.ngrok.app/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
 
-export const URL = HomeURL;
+export const URL = DevURL;
