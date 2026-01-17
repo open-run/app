c04d3685fee961fe3494ab7860947b13bee6fb49
windragon0807 <tmdfyd1995@gmail.com>
2026-01-17 22:55:23 +0900

    fix: 버전 업데이트 및 개발 URL 변경

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 const HomeURL = "https://open-run.vercel.app/";
-const DevURL = "https://e3ce9e0fee5f.ngrok.app/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
+const DevURL = "https://laughably-unblended-tiera.ngrok-free.dev/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
 
 export const URL = DevURL;
